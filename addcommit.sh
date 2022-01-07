0240627108dc192ad8c3be40da64fc1a0b41ecf2
Kamran Badirov <kamranbadirov@gmail.com>
2022-01-07 18:36:35 -0500

    added changes

--- addcommit.sh
+++ addcommit.sh
@@ -1,7 +1,18 @@
 !#/bin/bash
 
-git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "incorrect@email" ]; then
-     GIT_AUTHOR_EMAIL=kamranbadirov@gmail.com;
-     GIT_AUTHOR_NAME="kamrandb";
-     GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
-     GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
+git filter-branch --env-filter '
+WRONG_EMAIL="{ID}+{username}@users.noreply.github.com"
+NEW_NAME="kamrandb"
+NEW_EMAIL="kamranbadirov@gmail.com>"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
