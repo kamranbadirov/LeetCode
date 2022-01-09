3a4f65cedc8033e4fdfa9fcb16ce37491695d14b
kamrandb <kamranbadirov@gmail.com>
2022-01-09 00:08:40 -0500

    solved the question 977

--- 977.Squares of a Sorted Arrray.py
+++ 977.Squares of a Sorted Arrray.py
@@ -30,4 +30,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        for i in range(len(nums)):
+            nums[i] = nums[i]**2
+
+        nums.sort()
+        return nums
+
+
+
 
