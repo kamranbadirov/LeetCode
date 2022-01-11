602ba7632e10c127d0f5c9b7291e3e15e5d8087e
kamrandb <kamranbadirov@gmail.com>
2022-01-11 01:38:52 -0500

    solution of 283.Move Array

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -23,3 +23,18 @@ Constraints:
 
 Follow up: Could you minimize the total number of operations done?
 '''
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i] = nums[j]
+                i+=1
+        while i < len(nums):
+            nums[i] =0
+            i+=1
