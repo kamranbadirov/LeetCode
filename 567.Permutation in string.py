d80c6b018698c72fd7d68776517e24c32008f63f
kamrandb <kamranbadirov@gmail.com>
2022-01-17 01:16:03 -0500

    false solution

--- 567.Permutation in string.py
+++ 567.Permutation in string.py
@@ -21,3 +21,24 @@ Constraints:
 1 <= s1.length, s2.length <= 104
 s1 and s2 consist of lowercase English letters.
 '''
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        res = ""
+        for char in s2:
+            print(res)
+            if char in s1:
+                res += char
+            if res.count(char) > s1.count(char):
+                print(res)
+                res = res[-1]
+            if  (char not in s1):
+                res = ""
+            if len (res) == len(s1):
+                print(res)
+                return True
+        return False
+
+if __name__ == "__main__":
+    a = Solution()
+    print(a.checkInclusion("adc", "cdca"))
