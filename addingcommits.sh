cff01f0fac502dc1829472f33606742376801ce6
kamrandb <kamranbadirov@gmail.com>
2022-01-19 00:27:03 -0500

    testing git commands

--- addingcommits.sh
+++ addingcommits.sh
@@ -4,11 +4,12 @@
 
 for i in {1..25}
 do
-   if {{ $i % 2 == 0}}
-   then
-   touch testfile.txt
-   else rm -rf testfile.txt
-   fi
+    if ! (($i % 2)); then
+        touch testfile.txt
+    else
+        rm -rf testfile.txt
+    fi
+
    git add .
    git commit -m "testing git commands"
    git push
