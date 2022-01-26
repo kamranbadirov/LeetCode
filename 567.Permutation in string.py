db2c5a474492cc97f24e9164393d9912f744a193
kamrandb <kamranbadirov@gmail.com>
2022-01-26 00:22:34 -0500

    solved 567 string permutation

--- 567.Permutation in string.py
+++ 567.Permutation in string.py
@@ -23,33 +23,21 @@ s1 and s2 consist of lowercase English letters.
 '''
 
 class Solution:
-    # TODO: solve this again
     def checkInclusion(self, s1: str, s2: str) -> bool:
         s1_list = [0] * 26
         s2_list = [0] * 26
         if len(s1) > len(s2):
             return False
-
         for i in range(len(s1)):
-            s1_list[ord(s1[i])-ord('a')] = 1
-            s2_list[ord(s2[i])-ord('a')] = 1
+            s1_list[ord(s1[i])-ord('a')] += 1
+            s2_list[ord(s2[i])-ord('a')] += 1
         r=len(s1)
-        print("s1_list=",s1_list)
-        print("s2_list=",s2_list)
-
-        print("r = ",r)
-        if len(s2)> len(s1):
-            for j in range(len(s2[r:])):
-                if s1_list == s2_list:
-                    return True
-                else:
-                    print("s2_list=",s2_list)
-                    if ord(s2[j])!=ord(s2[r+j-1]):
-                        s2_list[ord(s2[j])-ord('a')] = 0
-                    s2_list[ord(s2[r+j])-ord('a')] = 1
-        print("final flag")
-        print('s1_list=',s1_list)
-        print('s2_list=',s2_list)
+        for j in range(len(s2[r:])):
+            if s1_list == s2_list:
+                return True
+            else:
+                s2_list[ord(s2[j])-ord("a")]-=1
+                s2_list[ord(s2[r+j])-ord('a')] += 1
         if s1_list == s2_list:
             return True
         return False
