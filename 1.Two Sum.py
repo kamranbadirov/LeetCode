c9e501912c82313ac671715b0cb00a1594a35ab7
kamrandb <kamranbadirov@gmail.com>
2022-02-03 23:56:19 -0500

    README.md

--- 1.Two Sum.py
+++ 1.Two Sum.py
@@ -39,3 +39,15 @@ class Solution:
             if (target-nums[i]) in res:
                 return [i, res[target-nums[i]]]
             res[nums[i]] = i
+
+
+    # newer solution by me:
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i,v in enumerate(nums):
+            if target - v in hash:
+                return [i, hash[target-v]]
+            else:
+                hash[v] = i
+
