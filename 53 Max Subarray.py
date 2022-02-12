f5cdad41526db3fd951c08c63e69975f806d5a7d
kamrandb <kamranbadirov@gmail.com>
2022-02-11 23:11:46 -0500

    second solution to 53

--- 53 Max Subarray.py
+++ 53 Max Subarray.py
@@ -29,3 +29,24 @@ class Solution:
             ans = max(ans, cur_sum)
         return ans
 
+    # second solution
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        f = 0
+        l = len(nums) - 1
+        if nums[l] >= nums[f]:
+            return nums[f]
+
+        while l > f:
+            m = f + (l - f) // 2
+            if nums[m] > nums[m+1]:
+                return nums[m+1]
+            if nums[m-1] > nums[m]:
+                return nums[m]
+            if nums[m] > nums[0]:
+                f = m + 1
+            else:
+                l = m - 1
+
