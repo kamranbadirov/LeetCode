1d0d28da5d4fe710112c69f92da140311e375462
kamrandb <kamranbadirov@gmail.com>
2022-02-12 00:47:51 -0500

    33 full solution

--- 33.Search in Rotated Sorted Array.py
+++ 33.Search in Rotated Sorted Array.py
@@ -16,3 +16,29 @@ class Solution:
         if target in nums:
             return nums.index(target)
         return -1
+
+    # full solution
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+
+        left = 0
+        right = len(nums)-1
+
+        while left<=right:
+            mid = left + (right-left)//2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid -1
+                else:
+                    left = mid + 1
+
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
