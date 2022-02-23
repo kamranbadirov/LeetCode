3c96666073e5f479ce4bc5f15646455b6ab70b11
kamrandb <kamranbadirov@gmail.com>
2022-02-22 23:26:10 -0500

    solution to 338

--- 338. Counting Bits.py
+++ 338. Counting Bits.py
@@ -33,3 +33,15 @@ Follow up:
 
 It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
 Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?'''
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        nums = range(n+1)
+
+        def count_bit(num):
+            count = 0
+            while num:
+                count += num & 1
+                num = num >> 1
+            return count
+        return [count_bit(x) for x in nums]
