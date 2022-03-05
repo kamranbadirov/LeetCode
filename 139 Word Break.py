6b3d90d29d6d140678a82dc29b604277bad34725
kamrandb <kamranbadirov@gmail.com>
2022-03-05 00:50:42 -0500

    139 correct solution

--- 139 Word Break.py
+++ 139 Word Break.py
@@ -29,3 +29,16 @@ Constraints:
 1 <= wordDict[i].length <= 20
 s and wordDict[i] consist of only lowercase English letters.
 All the strings of wordDict are unique.'''
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] *(len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s)-1, -1, -1):
+            for w in wordDict:
+                if (i+len(w)) <=len(s) and s[i:i+len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+        return dp[0]
